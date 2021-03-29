712909e277646b65badc2f1c3560766ea7e771ec
Nomit Kumar <kumarnomit@gmail.com>
2021-03-29 18:15:34 +0530

    Eslint Fixes

--- src/components/Svg.jsx
+++ src/components/Svg.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-   Search, ArrowHead, Trash, EditIcon,
+  Search, ArrowHead, Trash, EditIcon,
 } from './svg';
 
 const Svg = (props) => {
